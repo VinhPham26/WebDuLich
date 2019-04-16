1a153f39ae2f85f446cf17964242e44cb8dcce99
Thearesia26 <45289894+Thearesia26@users.noreply.github.com>
2019-04-16 10:11:08 +0700

    index

---
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Account objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (WebDBContext db = new WebDBContext())
+                {
+                    var obj = db.Accounts.Where(a => a.id.Equals(objUser.id) && a.pass.Equals(objUser.pass)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["ID"] = obj.id.ToString();
+                        return RedirectToAction("Web/Index");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+    }
+}

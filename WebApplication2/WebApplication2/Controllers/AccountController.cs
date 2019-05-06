b23923cfb05e2f9064d75660f788ab955b2e0d98
Thearesia26 <45289894+Thearesia26@users.noreply.github.com>
2019-05-06 22:42:09 +0700

    dk tài khoản của admin

--- WebApplication2/WebApplication2/Controllers/AccountController.cs
+++ WebApplication2/WebApplication2/Controllers/AccountController.cs
@@ -157,7 +157,49 @@ namespace WebApplication2.Controllers
                     return View(model);
             }
         }
+        [AllowAnonymous]
+        public ActionResult DKTK()
+        {
+            ViewBag.tk = new SelectList(context.Roles, "Name", "Name");
+            return View();
+        }
 
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DKTK(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771   
+                    // Send an email with this link   
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);   
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);   
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");   
+                    //Assign Role to user Here    
+                    await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
+                    return RedirectToAction("Index", "Home");
+                }
+                ViewBag.tk = new SelectList(context.Roles, "Name", "Name");
+                AddErrors(result);
+            }
+            if (!ModelState.IsValid)
+            {
+                ViewBag.tk = new SelectList(context.Roles, "Name", "Name");
+                return View();
+            }
+
+            // If we got this far, something failed, redisplay form   
+            return View(model);
+        }
         //
         // GET: /Account/Register
         [AllowAnonymous]
@@ -192,7 +234,7 @@ namespace WebApplication2.Controllers
                     await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
                     return RedirectToAction("Index","Home");
                 }
-                ViewBag.Name = new SelectList(context.Roles.Where(u => !u.Name.Contains("Admin")).ToList());
+                ViewBag.tk = new SelectList(context.Roles.Where(u => !u.Name.Contains("Admin")).ToList());
                 AddErrors(result);
             }
             if (!ModelState.IsValid)

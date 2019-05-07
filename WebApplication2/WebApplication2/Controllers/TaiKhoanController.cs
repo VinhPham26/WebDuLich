7aee267354988d6e3a86c789d8ea30dba7481134
Thearesia26 <45289894+Thearesia26@users.noreply.github.com>
2019-05-07 23:45:52 +0700

    update 3

---
+++ WebApplication2/WebApplication2/Controllers/TaiKhoanController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var result = from user in db.Users
+                         from role in db.Roles
+                         where role.Users.Any(r => r.UserId == user.Id)
+                         select new UserWithRole
+                         {
+                             fname = user.UserName,
+                             rname = role.Name
+                         };
+
+            return View(result);
+        }
+
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/UserWithRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class UserWithRole
+    {
+        public string fname { get; set; }
+        public string rname { get; set; }
+        //public string Email { get; set; }
+        //public string Role { get; set; }
+    }
+}
